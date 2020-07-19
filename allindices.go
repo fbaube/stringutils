08273eadf3bdc7d241e4bed4a6a8f0d9cb8164b7
Fred Baube <fbaube@welho.com>
2020-07-20 00:23:27 +0300

    Initial.

---
+++ allindices.go
@@ -0,0 +1,29 @@
+package stringutils
+
+import (
+	"fmt"
+	S "strings"
+)
+
+func AllIndices(in string, ch string) []int {
+	if len(ch) != 1 {
+		panic("Bad arg (len is not 1) to AllIndicesOf: " + ch)
+	}
+	if in == "" {
+		return nil
+	}
+	fmt.Printf("AllIndices: in<%s> ch<%s> \n", in, ch)
+	var length, idxToStart int
+	var idcs []int
+	for true {
+		length = S.Index(in[idxToStart:], ch)
+		fmt.Printf("idx: %v \n", length)
+		if length == -1 {
+			return idcs
+		}
+		idcs = append(idcs, idxToStart+length+1)
+		idxToStart += length + 1
+		// in = in[idxCume:]
+	}
+	return idcs
+}
