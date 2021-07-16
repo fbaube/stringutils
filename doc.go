2633ae132b43d3cce57b85a4d5f72ea584d834f4
Fred Baube <fbaube@welho.com>
2021-07-16 22:56:13 +0300

    Minor changes

--- doc.go
+++ doc.go
@@ -1,13 +1,13 @@
 // Package stringutils does string things.
 //
-// Some files in this directory use Markdown, so `godoc2md` will work on 'em.
+// Some files use Markdown, so `godoc2md` will work on 'em.
 //
-// This package does not use regular expressions anywhere. Also, this
-// package accommodates XML string quoting conventions by allowing the
-// use of either single quotes or double quotes.
+// This package does not use regular expressions anywhere. Also,
+// this package accommodates XML string quoting conventions by
+// allowing the use of either single quotes or double quotes.
 //
-// For functions that return a string or two plus a possibly-nil error,
-// they can be wrapped in functions Must(..) or Must2(..), respectively,
-// which either return successfully or a panic.
+// For functions that return a string or two plus a possibly-nil
+// error, they can be wrapped in functions Must(..) or Must2(..),
+// respectively, which either return successfully or a panic.
 //
 package stringutils
