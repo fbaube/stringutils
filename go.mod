bf7b5a2b5f11139ea9183741ababca025f07a5bd
Fred Baube <fbaube@iki.fi>
2024-03-18 08:41:03 +0200

    Add MU_type_DIRLIKE

--- go.mod
+++ go.mod
@@ -1,14 +1,17 @@
 module github.com/fbaube/stringutils
 
-go 1.21.6
+go 1.22
+
+toolchain go1.22.0
 
 require (
 	github.com/fatih/color v1.16.0
+	github.com/fbaube/humanbytes v0.0.0-20240227103335-a0b11501ad0e
 	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
 )
--- markuptype.go
+++ markuptype.go
@@ -3,6 +3,10 @@ package stringutils
 // MarkupType specifies the top-level, most general
 // type of content in a content entity: XML, HTML,
 // Markdown, or BIN (binary, "none of the above").
+//
+// There is also "Dirlike", for consistent handling
+// of this field in a tree environment. 
+// 
 // Note that altho HTML is "obviously" XML, HTML is
 // separated out here because HTML5 doesn't have a
 // really good definition (like DTD or XSD), and also
@@ -19,12 +23,24 @@ type MarkupType string
 
 const (
 	MU_type_UNK MarkupType = "UNK" // UNKNOWN
-
+	// MU_type_XML is assumed to be well-formed 
 	MU_type_XML  = "XML"
-	MU_type_HTML = "HTML" // Assumed to be HTML5
-	MU_type_MKDN = "MKDN" // Assumed to be CommonMark (or GFM?)
-	MU_type_BIN  = "BIN"  // Opaque
-	MU_type_SQL  = "SQL"  // Hey, why not eh 
+	// MU_type_HTML is assumed to be HTML5
+	MU_type_HTML = "HTML"
+	// MU_type_MKDN is assumed to be CommonMark (or GFM?)
+	MU_type_MKDN = "MKDN"
+	// MU_type_BIN is opaque 
+	MU_type_BIN  = "BIN"
+	// MU_type_SQL is, well, hey why not eh 
+	MU_type_SQL  = "SQL"
+	// MU_type_DIRLIKE is a placeholder for consistent handling
+	// (because of consistent problems in code): IsDirlike is
+	// a more general case of IsDir() - shorthand for "is not 
+	// possible to have own content" - but this can be more 
+	// formally defined as "ia/has link(s) to other stuff" 
+	// - i.e. it is a directory or symbolic link. Used by
+	// [ctoken.TypedRaw].
+	MU_type_DIRLIKE = "Dirlike" 
 )
 
 func (mt MarkupType) Echo() string {
@@ -32,7 +48,7 @@ func (mt MarkupType) Echo() string {
 }
 
 func (mt MarkupType) Info() string {
-	if len(mt) < 3 || len(mt) > 4 {
+	if len(mt) < 3 || len(mt) > 7 {
 		panic("Bad MU_type: " + mt)
 	}
 	switch mt {
@@ -46,6 +62,8 @@ func (mt MarkupType) Info() string {
 		return "Binary"
 	case MU_type_SQL:
 		return "SQL"
+	case MU_type_DIRLIKE:
+		return "Dirlike"
 	case MU_type_UNK:
 		return "Unknown"
 	}
