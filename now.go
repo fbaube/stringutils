bf4fec3db0ff5c8181d72d5660c7f2d6f14cae49
Fred Baube <fbaube@iki.fi>
2024-03-26 22:34:06 +0200

    Minor

--- now.go
+++ now.go
@@ -6,6 +6,15 @@ import (
 )
 
 func Now() string {
+/*
 	s := time.Now().UTC().Format(time.RFC3339)
 	return S.Replace(s, "T", "_", 1)
+	*/
+	return NowPlus()
 }
+
+func NowPlus() string {
+	return S.ToLower(time.Now().Local().
+	       Format("2006-01-02-Mon/15:04:05/-07"))
+}
+
