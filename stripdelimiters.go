128bd4b4834c09d4bb85d7a075be32b25d3171d4
Fred Baube <fbaube@iki.fi>
2025-01-20 14:41:31 +0200

    Simplify

--- stripdelimiters.go
+++ stripdelimiters.go
@@ -1,28 +1,29 @@
 package stringutils
 
 import (
-	"fmt"
 	S "strings"
+	"errors"
 )
 
-// StripDelimiters tries to remove corresponding characters from
-// both ends of the input string; `must`==`true` makes it mandatory.
-// From the resulting string, surrounding spaces get trimmed.
-//
-// If `must`==`true`, it returns an error if the delimiters are not found.
-// If `must`==`false`, an error is returned *iff* an argument was bad.
+// StripDelimiters tries to remove corresponding characters 
+// from both ends of the input string. From the resulting
+// string, surrounding spaces get trimmed. If an error is
+// returned, the original string is also returned. 
 //
 // About the meaning of "corresponding":
-// If `delimiters` is a single character (like a single or double quote),
-// it tries to remove that character from both ends of the input string.
-// If `delimiters` is two characters (like parentheses, braces, brackets),
-// they are assumed to form a left/right pair, and so the ends of the
-// input string are treated differently.
-func StripDelimiters(in string, delims string, must bool) (string, error) {
+//  - If `delimiters` is a single character (like a single or double quote),
+//    it tries to remove that character from both ends of the input string.
+//  - If `delimiters` is two characters (like parentheses, braces, brackets),
+//    they are assumed to form a left/right pair, and so the ends of the
+//    input string are treated differently.
+// .
+func StripDelimiters(in string, delims string) (string, error) {
 
 	in = S.TrimSpace(in)
-	if must && (in == "" || delims == "") {
-		return in, fmt.Errorf("Bad input to StripDelimiters")
+	strerr := "|"+in+"|"+delims+"|"
+
+	if in == "" || delims == "" { 
+		return in, errors.New("stripdelimiters bad input " + strerr)
 	}
 	var hasBeg, hasEnd bool
 	inLen := len(in)
@@ -33,13 +34,11 @@ func StripDelimiters(in string, delims string, must bool) (string, error) {
 		if hasBeg && hasEnd {
 			return S.TrimSpace(in[1 : inLen-1]), nil
 		}
-		if must {
-			return in, fmt.Errorf("StripDelimiters failed: |%s|%s|", in, delims)
-		}
-		return in, nil
+		return in, errors.New("stripdelimiters failed " + strerr)
 	}
 	if len(delims) != 2 {
-		return in, fmt.Errorf("StripDelimiters bad delim spec: |%s|", delims)
+		return in, errors.New(
+		          "stripdelimiters bad delim spec |" + delims + "|")
 	}
 	sBeg := string(delims[0])
 	sEnd := string(delims[1])
@@ -48,24 +47,18 @@ func StripDelimiters(in string, delims string, must bool) (string, error) {
 	if hasBeg && hasEnd {
 		return S.TrimSpace(in[1 : inLen-1]), nil
 	}
-	if must {
-		return in, fmt.Errorf("StripDelimiters failed: |%s|%s|", in, delims)
-	}
-	return in, nil
+	return in, errors.New("stripdelimiters failed " + strerr)
 }
 
-// StripQuotes tries to strip off matching XML quotes (i.e. either single
-// or double quotes). `success` indicates whether matching quotes were
-// found, and `must` makes the function fail if they are not found.
-func StripQuotes(in string, must bool) (out string, success bool) {
+// StripQuotes tries to strip off matching XML quotes (i.e. 
+// either single or double quotes). From the resulting string,
+// surrounding spaces get trimmed. If an error is returned,
+// the original string is also returned. 
+func StripQuotes(in string) (string, error) {
 	in = S.TrimSpace(in)
-	out, _ = StripDelimiters(in, "'", false)
-	out, _ = StripDelimiters(out, "\"", false)
-	if len(in)-2 == len(out) {
-		return out, true
-	}
-	if must {
-		return out, false
-	}
-	return out, true
+	out1, e1 := StripDelimiters(in, "'")
+	if e1 == nil { return out1, nil } 
+	out2, e2 := StripDelimiters(in, "\"")
+	if e2 == nil { return out2, nil }
+	return in, errors.New("stripquotes failed |" + in + "|")
 }
