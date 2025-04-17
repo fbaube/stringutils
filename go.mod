e5b66aefb1de7f6f59f6ef64e4bd13b8d815de13
Fred Baube <fbaube@iki.fi>
2025-04-17 18:53:06 +0200

    Housecleaning

--- go.mod
+++ go.mod
@@ -1,10 +1,12 @@
 module github.com/fbaube/stringutils
 
-go 1.23
+go 1.23.0
+
+toolchain go1.23.6
 
 require (
 	github.com/fatih/color v1.18.0
-	github.com/fbaube/dsmnd v0.0.0-20250111125333-2b80f885bfc2
+	github.com/fbaube/dsmnd v0.0.0-20250305202727-066656bb5c6a
 	github.com/fbaube/humanbytes v0.0.0-20250119091546-d58fbfdd444c
 	gopkg.in/yaml.v2 v2.4.0
 )
@@ -12,5 +14,5 @@ require (
 require (
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
 )
--- stringser.go
+++ stringser.go
@@ -1,10 +1,18 @@
 package stringutils
 
-// Stringser is a handy interface for content. Echo reproduces
-// the content, Info is a normal level of debuggery, and Debug
-// should be verbose. 
+// Stringser is a handy interface for content:
+//  - Echo reproduces the content (not guaranteed 100% tho) 
+//  - Infos is a normal, summary level of description
+//  - Debug should be verbose
+//
+// Notes: 
+//  - Policy re newlines is TBS
+//  - Debug can default to something like [fmt.Printf] ("%v")
+//  - [Infos] used to be Info, but that conflicted with
+//    [fs.DirEntry.Info] for struct [fileutils.FSItemer]
+// .
 type Stringser interface {
-	Echo() string
-	Info() string
+	Echo()  string
+	Infos() string
 	Debug() string
 }
